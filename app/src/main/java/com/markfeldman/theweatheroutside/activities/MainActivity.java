097d9ac1d76c46cd803f94783a27452819d71a95
Mark Feldman <markfeldman21@gmail.com>
2018-02-25 14:06:51 -0500

    Utilize Json class for data organization

--- app/src/main/java/com/markfeldman/theweatheroutside/activities/MainActivity.java
+++ app/src/main/java/com/markfeldman/theweatheroutside/activities/MainActivity.java
@@ -7,7 +7,10 @@ import android.widget.TextView;
 
 import com.markfeldman.theweatheroutside.R;
 import com.markfeldman.theweatheroutside.data.WeatherPreferences;
+import com.markfeldman.theweatheroutside.utilities.JsonUtils;
 import com.markfeldman.theweatheroutside.utilities.NetworkUtils;
+
+import org.json.JSONException;
 
 import java.io.IOException;
 
@@ -25,24 +28,32 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public class RetrieveWeatherOnline extends AsyncTask<String, Void, String>{
+    public class RetrieveWeatherOnline extends AsyncTask<String, Void, String[]>{
 
         @Override
-        protected String doInBackground(String... defWeather) {
+        protected String[] doInBackground(String... defWeather) {
             String defaultWeather = defWeather[0];
             String okHttpResponse = null;
+            String[] jsonResults = null;
             try {
                 okHttpResponse = NetworkUtils.okHttpDataRetrieval(defaultWeather);
+                jsonResults = JsonUtils.getSimpleWeatherStringsFromJson(MainActivity.this,okHttpResponse);
             } catch (IOException e) {
                 e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-            return okHttpResponse;
+            return jsonResults;
         }
 
         @Override
-        protected void onPostExecute(String okHttpResponse) {
-            weatherData.setText(okHttpResponse);
-            super.onPostExecute(okHttpResponse);
+        protected void onPostExecute(String[] jsonResults) {
+
+            if (jsonResults!=null){
+                for (String result : jsonResults){
+                    weatherData.append(result + "\n\n\n");
+                }
+            }
         }
     }
 }
