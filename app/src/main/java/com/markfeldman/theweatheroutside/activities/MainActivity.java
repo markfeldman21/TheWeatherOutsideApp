6217aba0dfc53eea6e8ed15a02208198ab1e9e50
Mark Feldman <markfeldman21@gmail.com>
2018-02-26 13:10:17 -0500

    Add polish to data retrieval in MainActivity

--- app/src/main/java/com/markfeldman/theweatheroutside/activities/MainActivity.java
+++ app/src/main/java/com/markfeldman/theweatheroutside/activities/MainActivity.java
@@ -3,6 +3,8 @@ package com.markfeldman.theweatheroutside.activities;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.markfeldman.theweatheroutside.R;
@@ -16,22 +18,50 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     private TextView weatherData;
+    private TextView errorMessage;
+    private ProgressBar progressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        errorMessage = (TextView)findViewById(R.id.tv_error_message_display);
+        progressBar = (ProgressBar) findViewById(R.id.pb_loading_indicator);
         weatherData = (TextView)findViewById(R.id.weather_data);
+        retrieveData();
+    }
+
+    private void retrieveData(){
         String defaultLocation = WeatherPreferences.getPreferredWeatherLocation(this);
         new RetrieveWeatherOnline().execute(defaultLocation);
+    }
 
+    private void errorMessage(){
+        errorMessage.setVisibility(View.VISIBLE);
+        weatherData.setVisibility(View.INVISIBLE);
+    }
+
+    private void displayWeather(){
+        errorMessage.setVisibility(View.INVISIBLE);
+        weatherData.setVisibility(View.VISIBLE);
     }
 
     public class RetrieveWeatherOnline extends AsyncTask<String, Void, String[]>{
 
         @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
         protected String[] doInBackground(String... defWeather) {
+
+            if (defWeather.length == 0) {
+                return null;
+            }
+
             String defaultWeather = defWeather[0];
             String okHttpResponse = null;
             String[] jsonResults = null;
@@ -48,6 +78,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String[] jsonResults) {
+            progressBar.setVisibility(View.INVISIBLE);
 
             if (jsonResults!=null){
                 for (String result : jsonResults){
