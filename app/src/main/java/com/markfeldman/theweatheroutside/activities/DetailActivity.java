3fefa3b2f0fbc42f28df114c1294bf630affd70f
Mark Feldman <markfeldman21@gmail.com>
2018-03-04 14:09:02 -0500

    Add share data to WeatherDetail Menu

--- app/src/main/java/com/markfeldman/theweatheroutside/activities/DetailActivity.java
+++ app/src/main/java/com/markfeldman/theweatheroutside/activities/DetailActivity.java
@@ -1,8 +1,11 @@
 package com.markfeldman.theweatheroutside.activities;
 
 import android.content.Intent;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,8 +28,40 @@ public class DetailActivity extends AppCompatActivity {
                 weatherData = checkForDataPassed.getStringExtra(Intent.EXTRA_TEXT);
                 retrievedData.setText(weatherData);
             }
+        }
+    }
 
+    public void shareMessage(){
+        String mimeType = "text/plain";
+        String shareTitle = "Share Weather";
+
+        if (weatherData!=null){
+            ShareCompat.IntentBuilder.from(this)
+                    .setType(mimeType)
+                    .setChooserTitle(shareTitle)
+                    .setText(weatherData)
+                    .startChooser();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.share_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.share_weather:{
+                shareMessage();
+                break;
+            }
         }
 
+        return super.onOptionsItemSelected(item);
     }
 }
