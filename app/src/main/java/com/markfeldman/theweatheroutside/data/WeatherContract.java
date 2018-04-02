faf6209bacbe63658d89f0a556df9cfb8df79a73
Mark Feldman <markfeldman21@gmail.com>
2018-04-02 12:55:29 -0400

    Added ContentProvider Uri to Contract Class

--- app/src/main/java/com/markfeldman/theweatheroutside/data/WeatherContract.java
+++ app/src/main/java/com/markfeldman/theweatheroutside/data/WeatherContract.java
@@ -1,12 +1,20 @@
 package com.markfeldman.theweatheroutside.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
+import java.net.URI;
+
 public class WeatherContract {
+    public static final String AUTHORITY = "com.markfeldman.theweatheroutside";
+    public  static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+    public static final String PATH_TASKS = "weather_table";
     private WeatherContract(){};
 
     //Inner Class For Each Seperate Table
     public static class WeatherData implements BaseColumns {
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_TASKS).build();
+
         public static final String TABLE_NAME = "weather_table";
         public static final String COLUMN_DATE = "date";
         public static final String COLUMN_DAY_OF_WEEK = "day";
