10cabc7acc7b3b8bf9ab08845accfa93672d948d
Mark Feldman <markfeldman21@gmail.com>
2018-04-17 13:35:30 -0400

    Use CursorLoader to retrieve individual weather in DetailActivity

--- app/src/main/java/com/markfeldman/theweatheroutside/activities/DetailActivity.java
+++ app/src/main/java/com/markfeldman/theweatheroutside/activities/DetailActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.nfc.Tag;
+import android.support.v4.app.LoaderManager;
 import android.support.v4.app.ShareCompat;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,12 +18,18 @@ import android.widget.Toast;
 
 import com.markfeldman.theweatheroutside.R;
 import com.markfeldman.theweatheroutside.data.WeatherContract;
+import com.markfeldman.theweatheroutside.data.WeatherPreferences;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
     private String weatherID;
+    private final String WEATHER_ID_FOR_LOADER_KEY = "weather_id";
+    private final int LOADER_ID = 33;
     private String weatherData;
     private TextView retrievedData;
     private final String BUNDLE_EXTRA_INT_ID = "Bundle Extra";
+    private String[] projection = {WeatherContract.WeatherData.COLUMN_DAY_OF_WEEK, WeatherContract.WeatherData.COLUMN_DATE,
+            WeatherContract.WeatherData.COLUMN_ICON_URL, WeatherContract.WeatherData.COLUMN_CONDITIONS, WeatherContract.WeatherData.COLUMN_HUMIDITY,
+            WeatherContract.WeatherData.COLUMN_HIGH_TEMPC, WeatherContract.WeatherData.COLUMN_HIGH_TEMPF};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +38,7 @@ public class DetailActivity extends AppCompatActivity {
 
         retrievedData = (TextView)findViewById(R.id.retrieved_data);
         weatherID = getIntent().getStringExtra(BUNDLE_EXTRA_INT_ID);
-        weatherData = retrieveWeatherFromDatabase(weatherID);
-
-        retrievedData.setText(weatherData);
-
+        startLoader(weatherID);
     }
 
     public void shareMessage(){
@@ -76,23 +82,47 @@ public class DetailActivity extends AppCompatActivity {
     }
 
 
-    private String retrieveWeatherFromDatabase(String weatherID){
-        String[] projection = {WeatherContract.WeatherData.COLUMN_DAY_OF_WEEK, WeatherContract.WeatherData.COLUMN_DATE,
-                WeatherContract.WeatherData.COLUMN_ICON_URL, WeatherContract.WeatherData.COLUMN_CONDITIONS, WeatherContract.WeatherData.COLUMN_HUMIDITY,
-                WeatherContract.WeatherData.COLUMN_HIGH_TEMPC, WeatherContract.WeatherData.COLUMN_HIGH_TEMPF};
+
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String weatherIDRetrieved = args.getString(WEATHER_ID_FOR_LOADER_KEY);
         Uri weatherQueryUri = WeatherContract.WeatherData.CONTENT_URI;
-        weatherQueryUri = weatherQueryUri.buildUpon().appendPath(weatherID).build();
+        weatherQueryUri = weatherQueryUri.buildUpon().appendPath(weatherIDRetrieved).build();
         String selectionWhere = WeatherContract.WeatherData._ID + "=?";
-        Cursor mCursor = getContentResolver().query(weatherQueryUri,projection,selectionWhere,new String[]{weatherID},null);
-
-        String dayOfWeek = mCursor.getString(mCursor.getColumnIndex(WeatherContract.WeatherData.COLUMN_DAY_OF_WEEK));
-        String weatherDate = mCursor.getString(mCursor.getColumnIndex(WeatherContract.WeatherData.COLUMN_DATE));
-        String humidity = mCursor.getString(mCursor.getColumnIndex(WeatherContract.WeatherData.COLUMN_HUMIDITY));
-        String iconURL = mCursor.getString(mCursor.getColumnIndex(WeatherContract.WeatherData.COLUMN_ICON_URL));
-        String conditions = mCursor.getString(mCursor.getColumnIndex(WeatherContract.WeatherData.COLUMN_CONDITIONS));
-        String highCelcius = mCursor.getString(mCursor.getColumnIndex(WeatherContract.WeatherData.COLUMN_HIGH_TEMPC));
-        String highFah = mCursor.getString(mCursor.getColumnIndex(WeatherContract.WeatherData.COLUMN_HIGH_TEMPF));
-
-        return dayOfWeek + " " + weatherDate + " " + humidity + " " + conditions + " " +  highCelcius + " " + highFah;
+        return new CursorLoader(this,weatherQueryUri,projection,selectionWhere,new String[]{weatherID},null);
     }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        String dayOfWeek = data.getString(data.getColumnIndex(WeatherContract.WeatherData.COLUMN_DAY_OF_WEEK));
+        String weatherDate = data.getString(data.getColumnIndex(WeatherContract.WeatherData.COLUMN_DATE));
+        String humidity = data.getString(data.getColumnIndex(WeatherContract.WeatherData.COLUMN_HUMIDITY));
+        String iconURL = data.getString(data.getColumnIndex(WeatherContract.WeatherData.COLUMN_ICON_URL));
+        String conditions =data.getString(data.getColumnIndex(WeatherContract.WeatherData.COLUMN_CONDITIONS));
+        String highCelcius = data.getString(data.getColumnIndex(WeatherContract.WeatherData.COLUMN_HIGH_TEMPC));
+        String highFah = data.getString(data.getColumnIndex(WeatherContract.WeatherData.COLUMN_HIGH_TEMPF));
+        weatherData = dayOfWeek + " " + weatherDate + " " + humidity + " " + conditions + " " +  highCelcius + " " + highFah;
+        retrievedData.setText(weatherData);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
+
+    private void startLoader(String weatherRowId){
+        Bundle queryBundle = new Bundle();
+        queryBundle.putString(WEATHER_ID_FOR_LOADER_KEY,weatherRowId);
+        LoaderManager loaderManager = getSupportLoaderManager();
+        Loader<String[]> loader = loaderManager.getLoader(LOADER_ID);
+        if (loader==null){
+            loaderManager.initLoader(LOADER_ID,queryBundle,this);
+        }else{
+            loaderManager.restartLoader(LOADER_ID,queryBundle,this);
+        }
+
+
+    }
+
 }
